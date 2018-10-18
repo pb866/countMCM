60ed38c70de519d0648a04feedc77dde90d62afc
Peter Bräuer <pb866@york.ac.uk>
2018-10-18 12:51:19 +0100

    Find RO2 in KPP sum, fixed MCM3v3.3.1 KPP file
    New function to retrieve RO2 species names used in the KPP RO2 sum 
    formula.
    Amending the script to find RO2 in the mechanism and the sum formula in
    the MCMv3.3.1, v3.2, and v3.1.
    Corrected KPP file for MCMv3.3.1.

--- src/countMCM.jl
+++ src/countMCM.jl
@@ -21,7 +21,7 @@ Columns are separated by an ampersand sign (`&`), use `&` also at the end of eac
 as last character. Missing species names can be put in curly braclets (`{...}`) and will
 be ignored in the database.
 """
-function readDB(filename::String="MCMv331species.db"; folder = "../DB")
+function readDB(filename::String="MCMspecies.db"; folder = "../DB")
   MCMdb = DataFrame()
   open(joinpath(Base.source_dir(), folder, filename)) do f
     lines = readlines(f)
@@ -81,17 +81,63 @@ function translateSPC(species::AbstractString, old_name::String, new_name::Strin
 end
 
 
-# MCMv3.3.1
+"""
+    readRO2(KPPfile::String="MCMv3.3.1.kpp"; folder::String="../KPPfiles")
+
+Retrieve all RO2 species (MCM names) from a default or specified `folder`/`KPPfile`
+and return the names as a Vector of strings.
+"""
+function readRO2(KPPfile::String="MCMv3.3.1.kpp"; folder::String="../KPPfiles")
+  # Initialise RO2 list
+  RO2 = String[]
+  open(joinpath(Base.source_dir(), folder, KPPfile), "r") do f
+    # Read KPP file and find section with RO2 summation
+    lines = readlines(f)
+    RO2index = findall(occursin.("C(", lines))
+    RO2lines = split.([replace(l, "&" => "") for l in lines[RO2index]], "+")
+    # Add RO2 species names to a list with out the KPP syntax for concentrations
+    for line in RO2lines, spc in line
+      println(spc)
+      spc = replace(spc, "C(ind_" => "")
+      spc = strip.(replace(spc, ")" => ""))
+      if spc ≠ ""  push!(RO2, spc)  end
+    end
+  end
+
+  return RO2
+end
+
+
+### MCMv3.3.1 ###
+# Read databases and KPP files to find and analyse RO2 in the MCM mechanism and the RO2 sum
 MCMdb = readDB()
 MCMv33species = readKPPspc()
 MCMv33gecko = translateSPC.(MCMv33species, "MCMname", "GECKO-A")
+RO2 = findall(occursin.("(OO.)", MCMv33gecko))
 
-# MCMv3.2
+# Define RO2 in the RO2 sum and the mechanism
+RO2mcm33 = MCMv33species[RO2]
+RO2sum33 = readRO2()
+
+
+### MCMv3.2 ###
+# Read databases and KPP files to find and analyse RO2 in the MCM mechanism and the RO2 sum
 MCMv32species = readKPPspc("MCMv3.2.kpp")
 MCMv32gecko = translateSPC.(MCMv32species, "MCMname", "GECKO-A", version = "v3.2")
+RO2 = findall(occursin.("(OO.)", MCMv32gecko))
 
-# MCMv3.1
+# Define RO2 in the RO2 sum and the mechanism
+RO2mcm32 = MCMv32species[RO2]
+RO2sum32 = readRO2("MCMv3.2.kpp")
+
+### MCMv3.1 ###
+# Read databases and KPP files to find and analyse RO2 in the MCM mechanism and the RO2 sum
 MCMv31species = readKPPspc("MCMv3.1.kpp")
 MCMv31gecko = translateSPC.(MCMv31species, "MCMname", "GECKO-A", version = "v3.1")
+RO2 = findall(occursin.("(OO.)", MCMv31gecko))
+
+# Define RO2 in the RO2 sum and the mechanism
+RO2mcm31 = MCMv31species[RO2]
+RO2sum31 = readRO2("MCMv3.1.kpp")
 
 println("done.")
